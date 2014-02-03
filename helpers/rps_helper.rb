d785d482b8e52c29fbf006fe9d3734115d5082d3
J. Paul Wetstein <jeep.wetstein@gmail.com>
2014-02-02 20:20:16 -0500

    Initial commit

---
+++ helpers/rps_helper.rb
@@ -0,0 +1,21 @@
+helpers do
+	
+	def winning_combos
+		return {Rock: "Scissors", Paper: "Rock", Scissors: "Paper"}
+	end
+	
+	def shoot_result throw, computer_throw
+		if throw.eql? computer_throw
+			return "Tie"
+		elsif winning_combos[throw.to_sym].eql? computer_throw
+			return "Win"
+		else
+			return "Lose"
+		end
+	end
+	
+	def computer_throw
+		["Rock", "Scissors", "Paper"].sample
+	end
+
+end
---
+++ rps_home.rb
@@ -0,0 +1,15 @@
+require 'sinatra'
+require './helpers/rps_helper.rb'
+
+set :port, 3000
+
+get '/' do
+	haml :index
+end
+
+post '/throw' do
+	@throw = params[:sign]
+	@computer_throw = computer_throw
+	@result = shoot_result @throw, @computer_throw
+	haml :throw
+end
