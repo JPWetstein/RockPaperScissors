fe29ae2b7830e340ddadff26ac42e076ec1d5855
J. Paul Wetstein <jeep.wetstein@gmail.com>
2014-02-03 07:34:57 -0500

    Fixed formatting on ruby files

--- helpers/rps_helper.rb
+++ helpers/rps_helper.rb
@@ -1,21 +1,19 @@
 helpers do
-	
-	def winning_combos
-		return {Rock: "Scissors", Paper: "Rock", Scissors: "Paper"}
-	end
-	
-	def shoot_result throw, computer_throw
-		if throw.eql? computer_throw
-			return "Tie"
-		elsif winning_combos[throw.to_sym].eql? computer_throw
-			return "Win"
-		else
-			return "Lose"
-		end
-	end
-	
-	def computer_throw
-		["Rock", "Scissors", "Paper"].sample
-	end
-
+  def winning_combos
+    return {Rock: "Scissors", Paper: "Rock", Scissors: "Paper"}
+  end
+  
+  def shoot_result throw, computer_throw
+  	if throw.eql? computer_throw
+    	return "Tie"
+  	elsif winning_combos[throw.to_sym].eql? computer_throw
+  		return "Win"
+  	else
+  		return "Lose"
+  	end
+  end
+  
+  def computer_throw
+  	["Rock", "Scissors", "Paper"].sample
+  end
 end
--- rps_home.rb
+++ rps_home.rb
@@ -5,12 +5,12 @@ require './helpers/rps_helper.rb'
 set :port, 3000
 
 get '/' do
-	haml :index
+  haml :index
 end
 
 post '/throw' do
-	@throw = params[:sign]
-	@computer_throw = computer_throw
-	@result = shoot_result @throw, @computer_throw
-	haml :throw
+  @throw = params[:sign]
+  @computer_throw = computer_throw
+  @result = shoot_result @throw, @computer_throw
+  haml :throw
 end
